77313a5b7c1007e6ad1b26da7198ff28e0c5f63c
HJQ <1051302489@qq.com>
2020-03-26 22:15:47 +0800

    完成活动筛选选择框，单击事件未绑定

--- miniprogram/pages/activityMain/activityMain.js
+++ miniprogram/pages/activityMain/activityMain.js
@@ -8,15 +8,64 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+
     //后端传入数据
-    realData : []
-      
-    
+    realData: [],
+
+    clubFilter: [
+      {
+        text: 'Club1',
+        value: 1
+      },
+      {
+        text: 'Club2',
+        value: 2
+      }
+    ],
+    dateFilter: [
+      {
+        text: '一天内',
+        value: 'b'
+      },
+      {
+        text: '三天内',
+        value: 'c'
+      },
+
+      {
+        text: '一周内',
+        value: 'd'
+      },
+
+      {
+        text: '一月内',
+        value: 'e'
+      }
+    ],
+    locationFilter : [
+      {
+        text: '文理学部',
+        value: 1
+      },
+      {
+        text: '信息学部',
+        value: 2
+      },
+      {
+        text: '工学部',
+        value: 3
+      },
+      {
+        text: '医学部',
+        value: 4
+      }
+    ]
+
+
 
   },
 
-  gotoActivity: function (e) {
+  gotoActivity: function(e) {
     // var index = e.currentTarget.testData.index;
     // var id = this.data.testData[index].id;
     // wx.setStorage({
@@ -35,19 +84,23 @@ Page({
     }
   },
 
+  item1click: function(e){
+    var index = parseInt(e.currentTarget.dataset.index);
+    console.log(index);
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     let that = this
     //数据容器
     let cloudData = []
     //调用云函数获取活动数据
     wx.cloud.callFunction({
       name: 'getActivityMainInfo',
-      success : function(res){
+      success: function(res) {
         console.log("云函数调用成功")
-        for (var activity of res.result.data.list){
+        for (var activity of res.result.data.list) {
           //获取每个活动的信息
           let obj = {
             "clubImg": activity.publishedActivities[0].clubImg,
@@ -61,59 +114,59 @@ Page({
         }
         //将数据容器赋值给小程序端
         that.setData({
-          realData : cloudData
+          realData: cloudData
         })
       }
     })
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
