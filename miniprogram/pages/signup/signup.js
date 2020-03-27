1484a01fdfbe4dd357cebdff3b438676448e8158
LOLOlobster <94967574@qq.com>
2020-03-28 00:38:42 +0800

    注册逻辑实现

--- miniprogram/pages/signup/signup.js
+++ miniprogram/pages/signup/signup.js
@@ -1,15 +1,15 @@
+/**
+ * 获取数据库引用：allUser集合
+*/
+const db = wx.cloud.database()
+const allUser = db.collection('allUser')
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     disabled: false, //按钮是否禁用
-    userName: '', //获取到的用户名栏中的值
-    userPwd: '',
-    userPwdAgain: '',
-    success: false,
-    state: ''
+    success: false
   },
   
   return_home: function (e) {
@@ -18,84 +18,96 @@ Page({
     })
 
   },
-  handleInputUserName: function (e) {
-    this.setData({
-      userName: e.detail.value
-    })
-  },
-  handleuserPwd: function (e) {
-    console.log(e);
-    this.setData({
-      userPwd: e.detail.value
-    })
-  },
-  handleuserPwdAgain: function (e) {
-    console.log(e);
-    this.setData({
-      userPwdAgain: e.detail.value
-    })
 
-  },
-  
-  submit: function (e) {
-    var that = this
-    var userName = that.data.userName;
-    var warn = null; //warn为当用户名为空时提示用户的文字，默认为空
-    if (userName == '') {
+  _handleSubmit: function (e) {
+
+    /**
+     * 获取用户输入
+    */
+    let inputName = e.detail.value.inputName
+    let inputPwd = e.detail.value.inputPwd
+    let repeatPwd = e.detail.value.repeatPwd
+
+    /**
+     * 验证输入合法性
+    */
+
+    if (inputName == '') {
       wx.showToast({
         title: '用户名不能为空',
+        icon : 'none',
         duration: 2000
       })
       return
-    }
-    else if (that.data.state == 1) {  //判断是否被注册
+    } 
+    if (inputPwd == '') {
       wx.showToast({
-        title: '用户名已被注册',
+        title: '密码不能为空',
+        icon: 'none',
         duration: 2000
       })
       return
-    }
-    else{
-    if (this.data.userPwd == '') {
-      wx.showToast({
-        title: '请输入密码',
-        duration: 2000
-      })
-      return
-    } else if (this.data.userPwdAgain != this.data.userPwd) {
+    } 
+    if (inputPwd !== repeatPwd) {
       wx.showToast({
         title: '两次密码不一致',
+        icon: 'none',
         duration: 2000
       })
       return
-    } else {
-      var that = this
-      var userName = that.data.userName;
-      wx.request({
-        url: getApp().globalData.baseUrl + '',
-        method: "POST",
-        data: {
-          userName: userName,
-          userPwd: that.data.userPwd
-        },
-        header: {
-          "content-type": "application/x-www-form-urlencoded"
-        },
-        success: function (res) {
-          wx.showToast({
-            title: '提交成功~',
-            icon: 'loading',
-            duration: 2000
-          })
-          console.log(res)
-          that.setData({
-            success: true
-          })
-        }
-      })
     }
-    }
+
+    /**
+     * 注册逻辑
+    */
+    allUser.where({
+      userName : inputName
+    }).get()
+    .then(res => {
+     if(res.data.length > 0){
+       wx.showToast({
+         title: '用户名已注册',
+         duration: 2000
+       })
+     }
+     else{
+       allUser.add({
+         data : {
+           userName : inputName,
+           userPwd : inputPwd
+         }
+       }).then(res =>{
+        wx.showModal({
+          title: '提示',
+          content: '注册成功',
+          success : function(res){
+            if(res.confirm){
+              wx.switchTab({
+                url: '../activityMain/activityMain',
+              })
+            }
+            else if(res.cancel){
+              wx.switchTab({
+                url: '../activityMain/activityMain',
+              })
+            }
+          }
+        })
+       })
+     }
+    })
+    
+    // wx.cloud.callFunction({
+    //   name : 'signUp',
+    //   data : {
+    //     inputName : inputName,
+    //     inputPwd : inputPwd
+    //   }
+    // }).then(res=>{
+    //   console.log(res.result)
+    // })
   },
+
   /**
    * 用户点击右上角分享
    */
