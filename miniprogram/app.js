476b1faf34c860710490fac28e69c14302ab96b5
LOLOlobster <94967574@qq.com>
2020-04-08 15:16:45 +0800

    添加登录相关的全局变量

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -11,12 +11,20 @@ App({
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
         //   如不填则使用默认环境（第一个创建的环境）
         // env: 'my-env-id',
+        env: 'mall-x3k10',
         traceUser: true,
       })
     }
 
     this.globalData = {
+<<<<<<< Updated upstream
       join:false
+=======
+      hasNameInput : false, //登录页面输入框是否有数据
+      hasPwdInput : false,   //用于控制登录界面登录按钮的启用
+      remPwd : false, //是否记住密码
+      autoLogin : false //是否自动登录
+>>>>>>> Stashed changes
     }
   }
 })
